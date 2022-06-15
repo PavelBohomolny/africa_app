6a20dffe2adbb58becbd34d2de336961ac09bcb8
PavelBohomolny <pbogomolny93@gmail.com>
2022-06-15 11:31:57 +0200

    Add navigation section and visual content

---
+++ Africa/App/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Pavel Bohomolnyi on 14/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
---
+++ Africa/App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Africa
+//
+//  Created by Pavel Bohomolnyi on 14/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Content")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
