4ef4942a6308b93f5dec4464e8212904f7a3930b
NexusUA <pbogomolny93@gmal.com>
2022-06-16 11:53:04 +0200

    Add JSON settings and fix Cover view

--- Africa/Extension/CodableBundleExtension.swift
+++ Africa/Extension/CodableBundleExtension.swift
@@ -12,5 +12,17 @@ extension Bundle {
         guard let url = self.url(forResource: file, withExtension: nil) else {
             fatalError("Failed to locate \(file) in bundle")
         }
+        
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) with bundle")
+        }
+        
+        let decoder = JSONDecoder()
+        
+        guard let loaded = try? decoder.decode([CoverImage].self, from: data) else {
+            fatalError("Failed to decode \(file) from bundle")
+        }
+        
+        return loaded
     }
 }
