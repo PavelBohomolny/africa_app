dbe5d1e5729ee4d5e08c608bcd5fffd1fd49b2a1
NexusUA <pbogomolny93@gmal.com>
2022-06-16 12:25:57 +0200

    Add animal list item view

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -11,13 +11,19 @@ struct ContentView: View {
     var body: some View {
         // MARK: - PROPERTIES
         
+        let animals: [Animal] = Bundle.main.decode("animals.json")
+        
         // MARK: - BODY
         NavigationView {
             List {
                 CoverImageView()
                     .frame(height: 300)
                     .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-            } //: list
+                
+                ForEach(animals) { animal in
+                    AnimalListItemView(animal: animal)
+                }
+            } //: List
             .navigationBarTitle("Africa", displayMode: .large)
         } //: Navigation
     }
---
+++ Africa/View/AnimalListItemView.swift
@@ -0,0 +1,53 @@
+//
+//  AnimalListItemView.swift
+//  Africa
+//
+//  Created by Pavel Bohomolnyi on 16/06/2022.
+//
+
+import SwiftUI
+
+struct AnimalListItemView: View {
+    // MARK: - PROPERTIES
+    
+    let animal: Animal
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        HStack(alignment: .center, spacing: 16) {
+            Image(animal.image)
+                .resizable()
+                .scaledToFill()
+                .frame(width: 90, height: 90)
+                .clipShape(
+                    RoundedRectangle(cornerRadius: 12)
+                )
+            
+            VStack(alignment: .leading, spacing: 8) {
+                Text(animal.name)
+                    .font(.title2)
+                    .fontWeight(.heavy)
+                    .foregroundColor(.accentColor)
+                
+                Text(animal.headline)
+                    .font(.footnote)
+                    .multilineTextAlignment(.leading)
+                    .lineLimit(2)
+                    .padding(.trailing, 8)
+            } //: VStack
+        } //: HStack
+    }
+}
+
+// MARK: - PREVIEW
+
+struct AnimalListItemView_Previews: PreviewProvider {
+    static let animals: [Animal] = Bundle.main.decode("animal.json")
+    
+    static var previews: some View {
+        AnimalListItemView(animal: animals[1])
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
